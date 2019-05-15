0ac4eecbb7b2c53e9393bf24126755a3840fba40
bigevilbeard <stuaclar@cisco.com>
2019-05-15 16:01:09 +0100

    updated for xr

--- iosxenapalmapi.click.py
+++ iosxenapalmapi.click.py
@@ -1,9 +1,9 @@
 import napalm
+import click
 import json
 
 class iosxenapalmapi(object):
     def __init__(self, hostname=None, username=None, password=None, optional_args=None):
-        # driver = napalm.get_network_driver('ios')
         driver = napalm.get_network_driver('ios-xr')
         self.connection = driver(hostname=hostname, username=username, password=password, optional_args={'port':8181})
 
@@ -13,8 +13,6 @@ class iosxenapalmapi(object):
 
     def disconnect(self):
         self.connection.close()
-
-
 
     def get_facts(self):
         """Retrieve and return network devices informatiom.
@@ -26,18 +24,37 @@ class iosxenapalmapi(object):
         self.disconnect()
         return facts
 
-    # def get_interfaces(self):
-    #     """Retrieve and return network devices informatiom.
-    #         ./iosxenapalmapi.py get_interfaces
-    #     """
-    #     self.connect()
-    #     facts = self.connection.get_interfaces()
-    #     self.disconnect()
-    #     return facts
+    def get_interfaces(self):
+        """Retrieve and return network devices informatiom.
+            ./iosxenapalmapi.py get_interfaces
+        """
+        self.connect()
+        facts = self.connection.get_interfaces()
+        self.disconnect()
+        return facts
 
 
 device = iosxenapalmapi("sbx-iosxr-mgmt.cisco.com", "admin", "C1sco12345")
-# device = iosxenapalmapi("ios-xe-mgmt.cisco.com", "root", "D_Vay!_10&")
-print(device.get_facts())
-# print(json.dumps(device.get_facts(), sort_keys=True, indent=4))
-# print(json.dumps(device.get_interfaces(), sort_keys=True, indent=4))
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def facts():
+    click.secho("Retrieving Information")
+    fact = json.dumps(device.get_facts(), sort_keys=True, indent=4)
+    click.echo(fact)
+
+
+@click.command()
+def interfaces():
+    click.secho("Retrieving Information")
+    interface = json.dumps(device.get_interfaces(), sort_keys=True, indent=4)
+    click.echo(interface)
+
+cli.add_command(facts)
+cli.add_command(interfaces)
+
+if __name__ == "__main__":
+    cli()
